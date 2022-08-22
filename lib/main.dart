bc3d1006290d148c960182a919ec424bfb9de073
Yeon0023 <yeon0023@e.ntu.edu.sg>
2022-08-23 03:41:57 +0800

    230822

--- lib/main.dart
+++ lib/main.dart
@@ -4,48 +4,23 @@ void main() {
   runApp(const MyApp());
 }
 
+//Make image border circular and add image inside
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.symmetric(
-              vertical: 100,
-              horizontal: 100,
-            ),
-            padding: const EdgeInsets.all(20),
-            transform: Matrix4.translationValues(10, -10, 0),
-            height: 300,
             width: 300,
-            //color: Colors.red,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              border: Border.all(
-                width: 2,
-                color: Colors.blueAccent,
-              ),
-              borderRadius: const BorderRadius.all(
-                Radius.elliptical(5, 10),
-              ),
-            ),
-            child: const Text(
-              'Hello world',
-              style: TextStyle(
-                color: Colors.green,
-                fontSize: 20,
-                fontFamily: 'roboto',
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                decoration: TextDecoration.lineThrough,
-                decorationColor: Colors.black,
-                decorationStyle: TextDecorationStyle.dashed,
-                letterSpacing: 10,
-                wordSpacing: 20,
+            height: 300,
+            color: Colors.deepOrange,
+            child: ClipOval(
+              child: Image.network(
+                'https://img.icons8.com/ios/200/000000/flutter.png',
+                fit: BoxFit.scaleDown,
+                color: Colors.blue[300],
               ),
             ),
           ),
@@ -54,3 +29,84 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+//Make image border circular and add image inside
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             decoration: BoxDecoration(
+//               color: Colors.deepOrange[300],
+//               borderRadius: BorderRadius.circular(150),
+//               image: const DecorationImage(
+//                   image: NetworkImage(
+//                       'https://img.icons8.com/ios/200/000000/flutter.png')),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// Use Image from asset folder (local computer)
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: double.infinity, //Entire screen width
+//             //width: 300,
+//             height: 600,
+//             color: Colors.deepOrange[300],
+//             child: Image.asset(
+//               'assets/external-heart-beat-world-cancer-awareness-flatart-icons-flat-flatarticons.png',
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+// Use Image from Web link 
+// class MyApp extends StatelessWidget {s
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+                //width: double.infinity, //Entire screen width
+//             width: 300,
+//             height: 600,
+//             color: Colors.deepOrange[300],
+//             child: Image.network(
+//               'https://img.icons8.com/ios/200/000000/flutter.png',
+//               alignment: Alignment.center,
+//               color: Colors.deepOrangeAccent,
+//               colorBlendMode: BlendMode.colorBurn,
+//               fit: BoxFit.fill,
+//               repeat: ImageRepeat.repeatY,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
